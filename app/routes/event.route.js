bb42fb85b8d28956ef1b155edaf497de3f2177bb
khiem20tc <khiem20tc@gitlab.com>
2021-03-21 09:23:34 +0700

    add info host in eventList

--- app/routes/event.route.js
+++ app/routes/event.route.js
@@ -16,9 +16,11 @@ router.get('/get', EventController.get)
 
 router.get('/getDetail', EventController.getDetail)
 
-router.put('/interact/:id', checkAuth, EventController.interact)
+//put
+router.post('/interact/:id', checkAuth, EventController.interact)
 
-router.put('/accept/:id', checkAuth, EventController.accept)
+//put
+router.post('/accept/:id', checkAuth, EventController.accept)
 
 router.delete('/delete/:id', checkAuth, EventController.remove)
 
